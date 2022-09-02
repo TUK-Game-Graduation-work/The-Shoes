56224e98bff2c2514e3317b5a6cd2aa6f7cc30c8
Seunghee-030 <senughee521@gmail.com>
2022-09-03 03:17:51 +0900

    2단 점프

--- The Shoes/Assets/02.Scripts/Character/PlayerMove.cs
+++ The Shoes/Assets/02.Scripts/Character/PlayerMove.cs
@@ -2,25 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class PlayerMove : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     public float moveSpeed = 10f;
     public float jumpPower = 10f;
 
-    private bool isJumping = false;
+    private bool isJumping = false; //점프 상태 확인
+    private int jumpCount = 2;      //점프 횟수 변경시 값 변경
 
     void Start()
     {
+        jumpCount = 0;
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //playerRigidbody.AddForce(Vector3.down * forceGravity);
-
         if (Input.GetKey(KeyCode.W))
             playerRigidbody.AddForce(0, 0, moveSpeed);
         if (Input.GetKey(KeyCode.A))
@@ -30,16 +28,28 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             playerRigidbody.AddForce(moveSpeed, 0, 0);
         jump();
-
-        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        //바닥에 닿았을 때(Ground tag를 가진 물체에 닿았을 때)     
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            jumpCount = 2;
+        }
     }
 
     void jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //점프 상태가 아니고 점프 카운트가 남아있을 때 실행
+        if (!isJumping && jumpCount > 0)
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 playerRigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
-            
+                jumpCount--;
+
+            }
         }
     }
 }
